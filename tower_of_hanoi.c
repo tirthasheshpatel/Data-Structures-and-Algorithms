a28cfe3d063307f39132c5fe2964e04f98919104
Tirth Patel <tirthasheshpatel@gmail.com>
2019-08-07 11:17:22 +0530

    Create Tower Of Hanoi in C
    Documentation will be added soon!

---
+++ tower_of_hanoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void tower_of_hanoi(int n, int fromPeg, int auxPeg, int toPeg)
+{
+    if(n == 1) printf("Move top disk from peg %d to peg %d\n", fromPeg, toPeg);
+    tower_of_hanoi(n-1, fromPeg, toPeg, auxPeg);
+    printf("Move top disk from peg %d to peg %d\n", fromPeg, toPeg);
+    tower_of_hanoi(n-1, auxPeg, fromPeg, toPeg);
+}
+
+int main()
+{
+    tower_of_hanoi(3, 1, 2, 3);
+}
