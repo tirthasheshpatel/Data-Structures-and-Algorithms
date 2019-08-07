c7947ec3b6cb140fc41056b91d0d412bec9023d0
Tirth Patel <tirthasheshpatel@gmail.com>
2019-08-07 11:22:00 +0530

    Add Tower of Hanoi in C

--- tower_of_hanoi.c
+++ tower_of_hanoi.c
@@ -4,9 +4,11 @@
 void tower_of_hanoi(int n, int fromPeg, int auxPeg, int toPeg)
 {
     if(n == 1) printf("Move top disk from peg %d to peg %d\n", fromPeg, toPeg);
-    tower_of_hanoi(n-1, fromPeg, toPeg, auxPeg);
-    printf("Move top disk from peg %d to peg %d\n", fromPeg, toPeg);
-    tower_of_hanoi(n-1, auxPeg, fromPeg, toPeg);
+    else{
+        tower_of_hanoi(n-1, fromPeg, toPeg, auxPeg);
+        printf("Move top disk from peg %d to peg %d\n", fromPeg, toPeg);
+        tower_of_hanoi(n-1, auxPeg, fromPeg, toPeg);
+    }
 }
 
 int main()
