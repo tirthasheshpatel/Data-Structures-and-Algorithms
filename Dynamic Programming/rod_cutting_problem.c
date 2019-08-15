6f791eb181e03c7362018b93496e36e0914d000a
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-08-15 06:09:07 +0530

    Add memoized and extended bottom-up approach.

--- Dynamic Programming/rod_cutting_problem.c
+++ Dynamic Programming/rod_cutting_problem.c
@@ -3,6 +3,14 @@
 #include <limits.h>
 #include <math.h>
 
+typedef struct pair Pair;
+
+struct pair
+{
+    int x;
+    int y;
+};
+
 int callbacks = 0;
 
 int max(int num1, int num2)
@@ -41,34 +49,64 @@ int memoized_cut_rod_top_down(int n, int rev[], int memo[])
 
 int memoized_cut_rod_bottom_up(int n, int rev[])
 {
-    int memo[n];
+    int memo[n+1];
     memo[0] = 0;
     int r;
-    for(int i=0;i<n;i++)
+    for(int i=1;i<=n;i++)
     {
         r = INT_MIN;
-        for(int j=0;j<i;j++)
+        for(int j=1;j<=i;j++)
         {
-            r = max(r, rev[j] + memo[i-j]);
+            r = max(r, rev[j-1] + memo[i-j]);
         }
         memo[i] = r;
     }
     return memo[n];
 }
 
+Pair extended_memoized_cut_rod_bottom_up(int n, int rev[])
+{
+    int memo[n+1];
+    int cuts[n+1];
+    Pair p = {-1,-1};
+    memo[0] = 0;
+    int r;
+    for(int i=1;i<=n;i++)
+    {
+        r = INT_MIN;
+        for(int j=1;j<=i;j++)
+        {
+            if(r < rev[j-1] + memo[i-j]){
+                r = rev[j-1] + memo[i-j];
+                cuts[i] = j;
+            }
+        }
+        memo[i] = r;
+    }
+    p.x = memo[n];
+    p.y = cuts[n];
+    return p;
+}
+
 int main()
 {
     int revenue[] = {1,5,8,9,10,17,17,20,24,30};
-    int n = 6;
-    printf("%d\n", cut_rod_top_down(n,revenue));
-    printf("%d\n", callbacks);
+    int n = 10;
+    printf("Top-Down  : %d\n", cut_rod_top_down(n,revenue));
+    printf("Callbacks : %d\n\n", callbacks);
 
     callbacks = 0;
 
     int memo[10];
     for(int i=0;i<10;i++) memo[i] = -1;
-    printf("%d\n", memoized_cut_rod_top_down(n,revenue, memo));
-    printf("%d\n", callbacks);
+    printf("Memoized top-down : %d\n", memoized_cut_rod_top_down(n,revenue, memo));
+    printf("Callbacks         : %d\n\n", callbacks);
 
-    printf("%d\n", memoized_cut_rod_bottom_up(n,revenue));
+    printf("Memoized bottom-up: %d\n\n", memoized_cut_rod_bottom_up(n,revenue));
+
+    for(int i=0;i<=n;i++)
+    {
+        Pair p = extended_memoized_cut_rod_bottom_up(i, revenue);
+        printf("Revenue: %d Optimum Cut: %d\n", p.x, p.y);
+    }
 }
